011d29591e6096cffa3358a9df0f16578796f82b
libesse <daidingcheng@gmail.com>
2021-02-13 13:43:58 +0800

    add the alpha focus highlight patch

--- st.h
+++ st.h
@@ -82,6 +82,7 @@ typedef union {
 
 void die(const char *, ...);
 void redraw(void);
+void tfulldirt(void);
 void draw(void);
 
 void externalpipe(const Arg *);
@@ -137,5 +138,5 @@ extern char *termname;
 extern unsigned int tabspaces;
 extern unsigned int defaultfg;
 extern unsigned int defaultbg;
-extern float alpha;
+extern float alpha, alphaUnfocused;
 extern const int boxdraw, boxdraw_bold, boxdraw_braille;
